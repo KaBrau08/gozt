2e145db9e6444570b3b571775682ee787b02810d
Braulio Herrera <braulio.apc@hotmail.com>
2018-04-26 14:54:02 -0600

    Test - 01

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,26 @@
 
 	// echo "<pre>".print_r($a, true)."</pre>";
 
-	phpinfo();
+	// phpinfo();
 
 ?>
-Hola Braulio :P
+
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+
+	<meta charset="UTF-8">
+
+	<title>Document</title>
+
+</head>
+
+<body>
+
+	<div class="ctd">Hola Braulio</div>
+	
+</body>
+
+</html>
